57b0f4761fd45028fc95f201c1396f45b9e36141
Sicario <thmas5952@gmail.com>
2022-06-26 12:03:34 -0300

    First Class finished

--- projeto_exemplo/Exemplo/src/Main.java
+++ projeto_exemplo/Exemplo/src/Main.java
@@ -1,3 +1,9 @@
 public class Main {
-    
+    public static void main(String[] args) {
+        System.out.println("--------------------------------------------");
+        System.out.println("Hello World! I am learning Java");
+    }
 }
+
+// psvm é como criar nosso método principal
+// primeiro objetivo, printar algo com sout[]
